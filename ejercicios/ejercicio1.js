26f8bb5c4eb3f38823c2ec088d280e90bdf30ac6
codesthenos <codesthenos@gmail.com>
2024-09-04 16:34:45 +0200

    Creo carperta ejercicios, donde en principio voy a meter todos los ejercicios menos el ultimo de la playlist que esta en la carpeta playlist y tambien el documento ejercicio1.js

---
+++ ejercicios/ejercicio1.js
@@ -0,0 +1,11 @@
+const usuario = {
+  nombre: 'codesthenos',
+  apellidos: 'ataraxia',
+  'fechas temas bootcamp': {
+    NodeJs: '2024-10-21',
+    Git: '2024-09-18',
+    React: '2024-12-16'
+  },
+  'busqueda activa': false
+}
+console.log(usuario["fechas temas bootcamp"].React)
