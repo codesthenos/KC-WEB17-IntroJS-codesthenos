247b17bb0942521ce62802e444aa02584de50d38
codesthenos <codesthenos@gmail.com>
2024-09-04 18:08:45 +0200

    Puestos ; en el ejercicio3-1 y terminado el ejercicio3-2

--- ejercicios/ejercicio3/ejercicio3-1.js
+++ ejercicios/ejercicio3/ejercicio3-1.js
@@ -4,30 +4,30 @@ const input1 = [
   'Videos',
   'capture',
   'mp4',
-]
+];
 
 const buildPath = (input) => {
   // recuperamos la sublista resultante de quitar el ultimo elemento del input y unimos los elementos en una cadena de caracteres metiendo '/' entre los elementos
-  const inputPath = input.slice(0, -1).join('/')
+  const inputPath = input.slice(0, -1).join('/');
   // recuperamos el ultimo elemento del input
-  const inputExtension = input.slice(-1)
+  const inputExtension = input.slice(-1);
   // unimos el path y la extension con un '.'
-  const inputPathExtension = `${inputPath}.${inputExtension}` // inputPath + '.' + inputExtension
-  return inputPathExtension
-}
+  const inputPathExtension = `${inputPath}.${inputExtension}`; // inputPath + '.' + inputExtension
+  return inputPathExtension;
+};
 
-const pathInput1 = buildPath(input1)
-console.log(pathInput1)
+const pathInput1 = buildPath(input1);
+console.log(pathInput1);
 
 // input2
 const input2 = [
   'CodinGame',
   'python',
   'py',
-]
+];
 // La funcion buildPath creada para el primer ejemplo me sirve exactamente igual para este ejemplo
-const pathInput2 = buildPath(input2)
-console.log(pathInput2)
+const pathInput2 = buildPath(input2);
+console.log(pathInput2);
 
 // input3
 const input3 = [
@@ -38,7 +38,7 @@ const input3 = [
   'useful',
   'pythonstuff',
   'py',
-]
+];
 // Igual, para este tercer ejemplo me sirve la funcion buildPath
-const pathInput3 = buildPath(input3)
-console.log(pathInput3)
+const pathInput3 = buildPath(input3);
+console.log(pathInput3);
---
+++ ejercicios/ejercicio3/ejercicio3-2.js
@@ -0,0 +1,28 @@
+// input
+const input = 10;
+
+const separateUnits = (input) => {
+  // transformo el input de Number a String
+  const stringInput = String(input);
+  // creo una lista con todos los digitos del input usando el spread operator
+  const listInput = [...stringInput];
+  // uno los elementos de la lista como en el ejercicio anterior
+  const unitsInput = listInput.join('-');
+
+  return unitsInput;
+};
+
+const unitsInput = separateUnits(input);
+console.log(unitsInput);
+
+// secondInput
+const secondInput = 1;
+// igual que en el ejericio anterior la funcion separateUnits me sirve para este ejemplo
+const unitsSecondInput = separateUnits(secondInput)
+console.log(unitsSecondInput)
+
+// thirdInput
+const thirdInput = 11234;
+// igual que en el ejericio anterior la funcion separateUnits me sirve para este ejemplo
+const unitsThirdInput = separateUnits(thirdInput)
+console.log(unitsThirdInput)
