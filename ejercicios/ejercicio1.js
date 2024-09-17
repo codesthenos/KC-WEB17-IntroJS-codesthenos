8821db0c83e1df90e89f6a49b1d0a509538a6b1d
codesthenos <codesthenos@gmail.com>
2024-09-17 23:28:19 +0200

    Arreglado ejercicio1, o eso creo

--- ejercicios/ejercicio1.js
+++ ejercicios/ejercicio1.js
@@ -1,11 +1,21 @@
 const usuario = {
   nombre: 'codesthenos',
   apellidos: 'ataraxia',
-  'fechas temas bootcamp': {
-    NodeJs: '2024-10-21',
-    Git: '2024-09-18',
-    React: '2024-12-16'
-  },
+  'modulos-bootcam-web17': [
+    {
+      nombre: 'NodeJs',
+      fecha: '2024-10-21'
+    },
+    {
+      nombre: 'Git',
+      fecha: '2024-09-18'
+    },
+    {
+      nombre: 'React',
+      fecha: '2024-12-16'
+    }
+  ],
   'busqueda activa': false
 }
-console.log(usuario["fechas temas bootcamp"].React)
+
+console.log(usuario['modulos-bootcam-web17'][2].fecha)
